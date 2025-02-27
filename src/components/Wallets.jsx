8a1a6c727f3dfe34dbe12a5c68f26de6bc36afb2
royce <royakhabhoa@gmail.com>
2025-02-27 13:23:52 +0100

    ...

--- src/components/Wallets.jsx
+++ src/components/Wallets.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import RecentTransactions from "./RecentTransactions";
-import FundWalletForm from "./fundWalletForm";
+import FundWalletForm from "./components/FundWalletForm.jsx";
 
 export default function Wallets() {
   const [modal, setModal] = useState(false);
